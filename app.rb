df458126e2edbec9d26dd6dab156a7cdebca509a
Art Witczak <art.bikes@gmail.com>
2011-03-13 16:48:01 -0700

    Adding some HAML views

--- app.rb
+++ app.rb
@@ -4,3 +4,23 @@ get('/'){ haml :index }
 get('/response'){ "Hello from the server" }
 get('/time'){ "The time is " + Time.now.to_s }
 get('/reverse'){ params[:word].reverse }
+
+__END__
+@@layout
+!!! 5
+%html
+  %head
+    %meta(charset="utf-8")
+    %title Sinatra Ajax
+  %body
+    %h1 Sinatra Ajax
+    = yield
+
+@@index
+%form#reverse(action="/reverse" method="POST")
+  %input#word(type="text" name="word")
+  %input(type="submit" value="Reverse")
+%ul
+  %li <a id="server" href="/response">Call server-side Function</a>
+  %li <a id="time" href="/time">Get the time</a>
+  %li <a id="amazing" href="#">Toggle Title</a>
