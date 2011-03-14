66ff1130489ada60652770936f403ce00a2faeb0
Art Witczak <art.bikes@gmail.com>
2011-03-13 17:30:21 -0700

    Ooops that should have been a post

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@
 get('/'){ haml :index }
 get('/response'){ "Hello from the server" }
 get('/time'){ "The time is " + Time.now.to_s }
-get('/reverse'){ params[:word].reverse }
+post('/reverse'){ params[:word].reverse }
 
 __END__
 @@layout
