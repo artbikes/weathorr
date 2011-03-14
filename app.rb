048668adde75ea28101d99a9b3147a3ef0f86a38
Art Witczak <art.bikes@gmail.com>
2011-03-14 01:38:52 -0700

    Breaking out the views

--- app.rb
+++ app.rb
@@ -10,28 +10,3 @@ get('/time'){ "The time is " + Time.now.to_s }
 post('/reverse'){ params[:word].reverse }
 
 __END__
-@@layout
-!!! 5
-%script(src="http://rightjs.org/hotlink/right.js")
-%script(src="/custom.js")
-%html
-  %head
-    %link{:rel => "stylesheet", :href => "/style.css", :type => "text/css", :media => "all"}
-    %meta(charset="utf-8")
-    %title Sinatra Ajax
-  %body
-    %h1 Sinatra Ajax
-    = yield
-
-@@index
-%h2 The Amazing Toggling Title
-%h2 and his Sidekick
-%form#reverse(action="/reverse" method="POST")
-  %input#word(type="text" name="word")
-  %input(type="submit" value="Reverse")
-%ul
-  %li <a id="server" href="/response">Call server-side Function</a>
-  %li <a id="time" href="/time">Get the time</a>
-  %li <a id="amazing" href="#">Toggle Title</a>
-%img(src="http://radar.weather.gov/Conus/Loop/centgrtlakes_loop.gif")
-#msg
