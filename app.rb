e846531043fff746d56446419cd088c90624c7e6
Art Witczak <art@quicksquid.com>
2011-05-03 23:49:10 -0700

    Now using Hpricot to grab data from XML file.

--- app.rb
+++ app.rb
@@ -1,6 +1,25 @@
-%w[rubygems sinatra haml sass heroku].each{ |gem| require gem }
+require 'rubygems'
+require 'sinatra'
+require 'haml'
+require 'sass' 
+require 'heroku' 
+require 'hpricot' 
+require 'open-uri'
 
-get('/'){ haml :index }
+get '/' do
+  doc = open("public/data/KBOS.xml") do |f|
+    Hpricot.XML(f)
+  end
+  @temp1 = (doc/"temp_f").inner_html
+  @temp2 = (doc/"temp_c").inner_html
+  @chill1 = (doc/"windchill_f").inner_html
+  @chill2 = (doc/"windchill_c").inner_html
+  @clouds = (doc/"weather").inner_html
+  @wind = (doc/"wind_dir").inner_html
+  @windspeed = (doc/"wind_mph").inner_html
+   
+  haml :index
+end
 get '/style.css' do
   content_type "text/css", :charset => "utf-8"
   sass :style
