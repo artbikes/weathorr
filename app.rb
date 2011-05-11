cb2f9cf78fcb8cefd48bc940cd5afa9be11fc510
Art Witczak <art.bikes@gmail.com>
2011-05-11 16:58:18 -0700

    Removing some vestigal code from example

--- app.rb
+++ app.rb
@@ -50,8 +50,3 @@ get '/style.css' do
   content_type "text/css", :charset => "utf-8"
   sass :style
 end
-get('/response'){ "Hello from the server" }
-get('/time'){ "The time is " + Time.now.to_s }
-post('/reverse'){ params[:word].reverse }
-
-__END__
